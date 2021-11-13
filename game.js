b7e3d7ee20779a276f29d6abe458ae89cbc27006
Vangul <kayameets@googlemail.com>
2021-11-13 23:45:59 +0100

    game.js and index.html

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded",function(){
 var startButton = document.querySelector("#startButton")
+var input = document.querySelector("#name-input")
+var content = document.querySelector("#content")
+startButton.addEventListener("click", function(){
+    content.innerHTML = "<h1>Prolog</h1>"
+})
 
-console.log(startButton)
 
 })
