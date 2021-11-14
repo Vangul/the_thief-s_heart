422d9c7682e57a764b63b158dd102379f1135ef1
Vangul <kayameets@googlemail.com>
2021-11-14 18:46:01 +0100

    game.js and index.html

--- game.js
+++ game.js
@@ -5,28 +5,47 @@ standingPlaza: {
         story: "Sie/er steht inmitten eines Platzes. Weit vor ihr/ihm ist ein Schafott aufgestellt und die Menschen eilen in Scharren auf den Platz. Denn eine Hinrichtung ist in dieser Stadt immer ein großes Spektakel.",
     choices:[
         {
-            choice: "Folge den Menschen und gehe selbst näher heran."
+            choice: "Folge den Menschen und gehe selbst näher heran.",
+            destination: "beforeSchafott"
         },
         {
-            choice: "Sieh verträumt in den Himmel und bleib stehen."
-        }    
-    ]}}
-
- //Plaza part}
-
-var story2 = {currentScence: "standingPlaza2",
-standingPlaza2: {
-    title: "Kapitel 1",
-    story: "Es passiert einiges.",
-    choices: [
-        {
-            choice: "Tue x."
-        },
-        {
-            choice: "Tue y."
+            choice: "Sieh verträumt in den Himmel und bleib stehen.",
+            destination: "standingPlaza2"
         }
     ]
-} }
+    },
+
+beforeSchafott: {
+         title: "Prolog",
+            story: "Es herrscht ein wildes Gedränge auf dem Weg zum Schafott. Nach einigen Minuten des durch die Menschen hindurchquetschen steht sie/er vor dem Schafott und sieht wie die Soldaten aufgereiht stehen und niemanden näher heran lassen.",
+        choices:[
+            {
+                choice: "Verfolge das Geschehen",
+                destination: "beforeSchafott2"
+            },
+            {
+                choice: "Wende dich ab und sie dir die Menschen an",
+                destination: "peopleBeforeShafott"
+            },
+        ]
+    },
+          
+standingPlaza2: {
+            title: "Prolog",
+            story: "Test",
+            choices:[
+                   {
+                       choice: "Test1",
+                       destination: "Test2"
+                   },
+                   {
+                       choice: "Test 3",
+                       destination: "Test4"
+                   },                        
+    ],
+}}
+
+
 
 
 
@@ -35,34 +54,40 @@ standingPlaza2: {
 document.addEventListener("DOMContentLoaded",function(){
 var startButton = document.querySelector("#startButton")
 var input = document.querySelector("#name-input")
-var content = document.querySelector("#content")
-var nextButton1 = document.querySelector("#nextButton1")
-startButton.addEventListener("click", function() {
-    content.innerHTML = `
+var inhalt = document.querySelector("#inhalt")
+startButton.addEventListener("click", renderScene) 
+})
+
+
+function renderScene() {
+    inhalt.innerHTML = `
     <h1>${story[story.currentScence].title}</h1>
     <p>${story[story.currentScence].story}</p>
     ${getInputs()}
-   
+    <button id= "next-button">Weiter</button>
     `
+    var button = document.querySelector("#next-button");
+    button.addEventListener("click", function(){
+        getInputValue()
+    })
+}
 
-})
-nextButton1.addEventListener("click", function(){
-    content.innerHTML = `
-    <hi>${story[story2.currentScence].title}</hi>
-    <p>${story[story2.currentScence].story}</p></>
-    <button>Weiter</button>
-    ${getInputs()}`
-})
-})
-
-
-
+function getInputValue(){
+    var inputs = document.querySelectorAll("input[type='radio']");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].checked){
+            story.currentScence = inputs[i].getAttribute('data-destination')
+            renderScene()
+        }
+    }
+}
 
 function getInputs(){
     var input = ""
     for(var i = 0; i < story[story.currentScence].choices.length; i++){
-        input += ` <div>
-        <input id="radio${i}" type="radio" name= "choices"/> 
+        input += ` 
+        <div>
+        <input data-destination = ${story[story.currentScence].choices[i].destination} id="radio${i}" type="radio" name= "choices"/> 
         <label for "radio${i}>${story[story.currentScence].choices[i].choice}</label>
         </div>`
     }
